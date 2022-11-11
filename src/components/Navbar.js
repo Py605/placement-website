2ad10a991324075fb7d5e45082964da7bd21cd18
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-11-11 18:15:10 +0530

    Added Authentication System + Fixed Navbar Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { FaBars, FaFacebook, FaTwitter, FaInstagram, FaWhatsapp } from 'react-icons/fa';
+import React, { useLayoutEffect, useRef, useState, useEffect } from 'react';
+import {
+  FaBars,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaWhatsapp,
+} from 'react-icons/fa';
 import logo from '../assets/logo/dgi-big-logo.jpg';
-import "../index.css"
+import '../index.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
@@ -18,9 +25,20 @@ const Navbar = () => {
       linksContainerRef.current.style.height = '0px';
     }
   }, [showLinks]);
-  
+
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } =
+    useAuth0();
+
+  const ref = useRef(null);
+
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    setWidth(window.screen.width);
+  }, [window.screen.width]);
+
   return (
-    <nav>
+    <nav ref={ref}>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="logo" alt="logo" />
@@ -42,34 +60,79 @@ const Navbar = () => {
             <li key="4">
               <a href="/contact">Contact</a>
             </li>
+            {(width <= 816) ? (isAuthenticated ? (
+              <li key="5">
+                <a>
+                  <button
+                    className="authenticate-btn"
+                    onClick={() => logout({ returnTo: window.location.origin })}
+                  >
+                    Log Out
+                  </button>
+                </a>
+              </li>
+            ) : (
+              <li key="5">
+                <a>
+                  <button
+                    className="authenticate-btn"
+                    onClick={() => loginWithRedirect()}
+                  >
+                    Log In
+                  </button>
+                </a>
+              </li>
+            ))
+          :
+          <li></li>
+          }
           </ul>
         </div>
-        <ul className="social-icons">
-          <li key="1">
-            <a href="https://www.facebook.com/Doongroupofinstitution/">
-              <FaFacebook />
-            </a>
-          </li>
-          <li key="2">
-            <a href="https://twitter.com/DoonInstitute">
-              <FaTwitter />
-            </a>
-          </li>
-          <li key="3">
-            <a href="https://www.instagram.com/doon_group_of_institution/">
-              <FaInstagram />
-            </a>
-          </li>
-          <li key="4">
-            <a href="https://web.whatsapp.com/send?phone=917466000340&text=Hello,%20I%20am%20intrested%20in%20to%20know%20more%20about%20the%20college%20and%20the%20courses.">
-              <FaWhatsapp />
-            </a>
-          </li>
-        </ul>
+        
+        <div>
+          <ul className="social-icons">
+            <li key="1">
+              <a href="https://www.facebook.com/Doongroupofinstitution/">
+                <FaFacebook />
+              </a>
+            </li>
+            <li key="2">
+              <a href="https://twitter.com/DoonInstitute">
+                <FaTwitter />
+              </a>
+            </li>
+            <li key="3">
+              <a href="https://www.instagram.com/doon_group_of_institution/">
+                <FaInstagram />
+              </a>
+            </li>
+            <li key="4">
+              <a href="https://web.whatsapp.com/send?phone=917466000340&text=Hello,%20I%20am%20intrested%20in%20to%20know%20more%20about%20the%20college%20and%20the%20courses.">
+                <FaWhatsapp />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className='authentication'>
+          {(width <= 816) ? "" : isAuthenticated ? (
+            <button
+              className="authenticate-btn"
+              onClick={() => logout({ returnTo: window.location.origin })}
+            >
+              Log Out
+            </button>
+          ) : (
+            <button
+              className="authenticate-btn"
+              onClick={() => loginWithRedirect()}
+            >
+              Log In
+            </button>
+          )}
+        </div>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-mu6sbqzfjnhfxr8d.us.auth0.com"
+    clientId="QECtHFcasVAbl1939kzKcbzq5f5Deffu"
+    redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
